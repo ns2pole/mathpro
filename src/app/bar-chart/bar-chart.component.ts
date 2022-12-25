8e86a3e0c24b3be49291e1dd0e51e2be52f9292b
中村俊介 <nakamura@feliseed.co.jp>
2022-12-25 17:30:27 +0900

    sortの部分を作成中。

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -18,7 +18,7 @@ export class BarChartComponent {
     scales: {
       x: {},
       y: {
-        min: 10
+        min: 0
       }
     },
     plugins: {
@@ -37,32 +37,15 @@ export class BarChartComponent {
   ];
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: [ '', '', '', '', '', '', '' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [ 1, 2, 3, 4 ,5 ,6 ,7 ], label: '' }
     ]
   };
 
-  // events
-  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
-  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
-    console.log(event, active);
-  }
-
   public randomize(): void {
-    // Only Change 3 values
     this.barChartData.datasets[0].data = [
-      Math.round(Math.random() * 100),
-      59,
-      80,
-      Math.round(Math.random() * 100),
-      56,
-      Math.round(Math.random() * 100),
-      40 ];
+      2, 2, 3, 4, 5, 6, 7 ];
 
     this.chart?.update();
   }
