7c1cffbe73971770e933dc92d22285bb4e89e3e5
中村俊介 <nakamura@feliseed.co.jp>
2022-12-25 18:27:55 +0900

    バブルソートの部分を雑だが作った。

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -37,15 +37,44 @@ export class BarChartComponent {
   ];
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '', '', '', '', '', '', '' ],
+    labels: [ '', '', '', '', '', '', '', '', '', '' ],
     datasets: [
-      { data: [ 1, 2, 3, 4 ,5 ,6 ,7 ]}
+      { data: this.getPermutationOf(10)}
     ]
   };
 
   public randomize(): void {
-    this.barChartData.datasets[0].data = [
-      2, 2, 3, 4, 5, 6, 7 ];
+    this.barChartData.datasets[0].data = this.getPermutationOf(10);
     this.chart?.update();
+  }
+
+  private getPermutationOf(n: number): number[] {
+    let arr = Array.from(Array(n).keys());
+    for (let i = n - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    arr = arr.map(x => x + 1)
+    return arr;
+  }
+
+  public async bubbleSort(): Promise<void> {
+    for(let i = 0; i < 10 ; i++) {
+      this.getSortedArr(this.barChartData.datasets[0].data as number[], i);
+      this.chart?.update();  
+      await new Promise(resolve => setTimeout(resolve, 1000)) // 3秒待つ
+    }
+  }
+
+  public getSortedArr(arr: number[], i: number): void {
+    // for (let i = 0; i < arr.length - 1; i++) {
+      for (let j = arr.length - 1; j > i; j--) {
+        if (arr[j] < arr[j - 1]) {
+          [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        }
+      }
+      console.log(arr);
+    // }
+    // return arr;
   }
 }
