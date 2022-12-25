43877282f86ed12335e40c0e8e8985df610f0e09
中村俊介 <nakamura@feliseed.co.jp>
2022-12-25 15:11:29 +0900

    棒グラフを試した

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { TopComponent } from './top/top.component';
 import { ContactComponent } from './contact/contact.component';
 import { FeeComponent } from './fee/fee.component';
 import { AboutComponent } from './about/about.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 // Routing を行う対象のコンポーネントを管理する
 // path にセットした文字列にマッチしたURLが指定されると、対になっているコンポーネントが表示される
@@ -28,10 +30,12 @@ const ROUTE_TABLE: Routes = [
     AboutComponent,
     FeeComponent,
     ContactComponent,
+    BarChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgChartsModule,
     RouterModule.forRoot(ROUTE_TABLE), // 追加. routing の情報を登録する
   ],
   providers: [],
