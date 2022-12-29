e3fdc95fd3f549e039ec9259b42661c672aca9f3
中村俊介 <nakamura@feliseed.co.jp>
2022-12-29 11:18:52 +0900

    一旦動かすだけ動かしたがやりたいことと違った

--- src/app/top/field/field.component.ts
+++ src/app/top/field/field.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
-import {state, style} from '@angular/animations';
+import {state, style, transition, animate, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-field',
   templateUrl: './field.component.html',
   styleUrls: ['./field.component.css'],
   animations: [
-    state(
-      'blue',
-      style({
-      background: 'blue',
-      })
-    ),
-    state(
-      'red',
-      style({
-      background: 'red',
-      })
-    ),
-  ],
+    trigger('changeColor', [
+      state(
+        'white',
+        style({
+        background: 'white',
+        })
+      ),
+      state(
+        'blue',
+        style({
+        background: 'blue',
+        })
+      ),
+      state(
+        'red',
+        style({
+        background: 'red',
+        })
+      ),
+      transition('white => blue', [animate('1s')]),
+      transition('blue => white', [animate('1s')]),
+    ])
+  ]
 })
 
 export class FieldComponent {
-  isBlue = true;
+  isWhite = true;
   changeColor() {
-    this.isBlue = !this.isBlue;
+    this.isWhite = !this.isWhite;
   }
 }
