4ec7f2295c401eddefaa2a676c7ca2059da9a79e
ns2pole <nakamura@feliseed.co.jp>
2023-01-05 00:26:39 +0900

    使えるであろうテストをコミット

--- src/app/top/breadth-first-search/breadth-first-search.component.spec.ts
+++ src/app/top/breadth-first-search/breadth-first-search.component.spec.ts
@@ -1,13 +1,18 @@
+import { IsAdjacent } from './IsAdjacent';
 import { Plane } from './Plane';
-
+import { Space } from './Space';
 
 describe('BreadthFirstSearchComponent', () => {
 
-
-
   describe('sampleClassのテスト', () => {
     test('sample1', () => {
-      expect(1).toBe(1);
+      let ad : Array<Array<IsAdjacent>> = Plane.getAdjacentMatrix(
+        [[Space.isObstacle, Space.isStart, Space.isVacant, Space.isVacant],
+        [Space.isObstacle, Space.isVacant, Space.isVacant, Space.isVacant],
+        [Space.isObstacle, Space.isObstacle, Space.isVacant, Space.isObstacle],
+        [Space.isObstacle, Space.isObstacle, Space.isVacant, Space.isVacant]
+        ]);
+      expect(ad).toBe([]);
     });
   });
 });
