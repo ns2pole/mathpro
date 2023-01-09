bb51b0111b9de42d434f1e0bea486c80df5b8417
ns2pole <spaceofstar2@gmail.com>
2023-01-09 21:23:04 +0900

    幅優先探索を作成中。ごちゃごちゃしてきた。

--- src/app/top/breadth-first-search/Vertex.ts
+++ src/app/top/breadth-first-search/Vertex.ts
@@ -35,10 +35,45 @@ export class Vertex {
     }
     return edges;
   }
-  
-    
-  getPathTo(v : Vertex) : Array<Vertex> {
-    return [];
+
+  getIdAndAdjacentVertexIds(ad : AdjacentMatrix) : [number, Array<number>] {
+    return [this.id, this.getAdjacentVertexIdsBy(ad)]
   }
 
+  getIdsSequenceOfBreadFirstlyPathTo(id : number, ad : AdjacentMatrix) : Array<Array<[number, Array<number>]>> {
+    let sequenceOfIdsOnPath : Array<Array<[number, Array<number>]>> = []
+    if(id == this.id) {
+      return []
+    } else {
+      let initPair : [number, Array<number>] = this.getIdAndAdjacentVertexIds(ad)
+      sequenceOfIdsOnPath.push([initPair])
+      let ids : number[] = initPair[1]
+      while(ids.includes(id)) {
+        sequenceOfIdsOnPath.push(Vertex.evolute(sequenceOfIdsOnPath[length - 1], ad)) 
+      }
+      return sequenceOfIdsOnPath;
+    }
+  }
+
+  static evolute(searchSequence : Array<[number, Array<number>]>, ad : AdjacentMatrix) : Array<[number, Array<number>]> {
+    let evolutedSequence : Array<[number, Array<number>]> = []
+    let ids = []
+    for(let i : number = 0; i < searchSequence.length; i++) {
+      ids.push(searchSequence[i][1])
+    }
+    ids = ids.flat()
+    for(let i : number = 0; i < ids.length; i++) {
+      let vertex : Vertex = new Vertex(ids[i])
+      evolutedSequence.push(vertex.getIdAndAdjacentVertexIds(ad))
+    }
+    return evolutedSequence;
+  }
+
+  getIdsOfBreadFirstlyPathTo(id : number, ad : AdjacentMatrix) : Array<number> {
+    let sequenceOfIdsOnPath : Array<Array<number>> = []
+    for(let i : number = 0; i < ad.length; i++) {
+      sequenceOfIdsOnPath.push(this.getAdjacentVertexIdsBy(ad))
+    } 
+    return [];
+  }
 }
