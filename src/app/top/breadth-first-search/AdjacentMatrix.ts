9fba3f29185611250c8eb87afe998e43f94a4109
ns2pole <spaceofstar2@gmail.com>
2023-01-09 13:40:05 +0900

    隣接行列のための関数を追加

--- src/app/top/breadth-first-search/AdjacentMatrix.ts
+++ src/app/top/breadth-first-search/AdjacentMatrix.ts
@@ -1,11 +1,13 @@
 import { Labyrinth2D } from './Labyrinth2D';
 import { IsAdjacent} from './Union';
-import { isInside } from './FunctionModule';
+import { isInside, isBottomSide, isTopSide, isLeftSide, isRightSide, isCorner, isTopLeftCorner, isTopRightCorner, isBottomLeftCorner, isBottomRightCorner } from './FunctionModule';
+
+
 export class AdjacentMatrix extends Array<Array<IsAdjacent>> {
 	constructor() {
 		super();
 	}
-	
+
 	static getAdjacentMatrixFor(lab : Labyrinth2D) : AdjacentMatrix {
 		let adjacentMatrix : AdjacentMatrix = new AdjacentMatrix();
 		for( let i :number = 0; i < lab.getIdCount(); i++) {
@@ -20,33 +22,111 @@ export class AdjacentMatrix extends Array<Array<IsAdjacent>> {
 	}
 
 	setAdjacentInfoFor(map : Labyrinth2D, row: number, column: number): void {
-		if(isInside(map, row, column) && map[row][column] == "isObstacle") {
+		if(isTopSide(map, row, column) && !isCorner(map, row, column) && map[row][column] == "isObstacle") {
 			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row, column + 1)] = "NotAdjacent";
-			this[map.getVertexIdOf(row, column + 1)][map.getVertexIdOf(row, column)] = "NotAdjacent";		
-			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row, column - 1)] = "NotAdjacent";
-			this[map.getVertexIdOf(row, column - 1)][map.getVertexIdOf(row, column)] = "NotAdjacent";
+			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row, column + 1)] = "NotAdjacent";			
+		}
+		if(isInside(map, row, column) && map[row][column] != "isObstacle") {
+			
+		}
+	}
+
+	// setAdjacentInfoForEveryDirection(map : Labyrinth2D, row: number, column: number): void {
+	setAdjacentInfoForAbove(map : Labyrinth2D, row: number, column: number): void {
+		if(map[row][column] == "isObstacle") {
 			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row + 1, column)] = "NotAdjacent";
 			this[map.getVertexIdOf(row + 1, column)][map.getVertexIdOf(row, column)] = "NotAdjacent";
-			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row - 1, column)] = "NotAdjacent";
-			this[map.getVertexIdOf(row - 1, column)][map.getVertexIdOf(row, column)] = "NotAdjacent";
-		} 
-		if(isInside(map, row, column) && map[row][column] != "isObstacle") {
+		} else {
 			if(map[row][column + 1] != "isObstacle") {
 				this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row, column + 1)] = "Adjacent";
 				this[map.getVertexIdOf(row, column + 1)][map.getVertexIdOf(row, column)] = "Adjacent";
 			}
+		}
+	}
+	setAdjacentInfoForBelow(map : Labyrinth2D, row: number, column: number): void {
+		if(map[row][column] == "isObstacle") {
+			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row + 1, column)] = "NotAdjacent";
+			this[map.getVertexIdOf(row + 1, column)][map.getVertexIdOf(row, column)] = "NotAdjacent";
+		} else {
 			if(map[row][column - 1] != "isObstacle") {
 				this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row, column - 1)] = "Adjacent";
 				this[map.getVertexIdOf(row, column - 1)][map.getVertexIdOf(row, column)] = "Adjacent";
 			}
+		}
+	}
+
+	setAdjacentInfoForLeft(map : Labyrinth2D, row: number, column: number): void {
+		if(map[row][column] == "isObstacle") {
+			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row + 1, column)] = "NotAdjacent";
+			this[map.getVertexIdOf(row + 1, column)][map.getVertexIdOf(row, column)] = "NotAdjacent";
+		} else {
+			if(map[row - 1][column] != "isObstacle") {
+				this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row - 1, column)] = "Adjacent";
+				this[map.getVertexIdOf(row - 1, column)][map.getVertexIdOf(row, column)] = "Adjacent";
+			}	
+		}
+	}
+
+	setAdjacentInfoForRight(map : Labyrinth2D, row: number, column: number): void {
+		if(map[row][column] == "isObstacle") {
+			this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row + 1, column)] = "NotAdjacent";
+			this[map.getVertexIdOf(row + 1, column)][map.getVertexIdOf(row, column)] = "NotAdjacent";
+		} else {
 			if(map[row + 1][column] != "isObstacle") {
 				this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row + 1, column)] = "Adjacent";
 				this[map.getVertexIdOf(row + 1, column)][map.getVertexIdOf(row, column)] = "Adjacent";
 			}
-			if(map[row - 1][column] != "isObstacle") {
-				this[map.getVertexIdOf(row, column)][map.getVertexIdOf(row - 1, column)] = "Adjacent";
-				this[map.getVertexIdOf(row - 1, column)][map.getVertexIdOf(row, column)] = "Adjacent";
-			}
 		}
+	}
+
+	setAdjacentInfoForTopLeftCorner(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+	}
+
+	setAdjacentInfoForTopRightCorner(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
+	}
+
+	setAdjacentInfoForBottomLeftCorner(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+	}
+
+	setAdjacentInfoForBottomRightCorner(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
+	}
+
+	setAdjacentInfoForTopInSide(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
+	}
+
+	setAdjacentInfoForBottomInSide(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
+	}
+
+	setAdjacentInfoForLeftInSide(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+	}
+
+	setAdjacentInfoForRightInSide(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
+	}
+
+	setAdjacentInfoForInside(map : Labyrinth2D, row: number, column: number): void {
+		this.setAdjacentInfoForAbove(map, row, column);
+		this.setAdjacentInfoForBelow(map, row, column);
+		this.setAdjacentInfoForRight(map, row, column);
+		this.setAdjacentInfoForLeft(map, row, column);
 	}
 }
--- src/app/top/breadth-first-search/FunctionModule.ts
+++ src/app/top/breadth-first-search/FunctionModule.ts
@@ -8,6 +8,18 @@ export const isBottomSide : (map : Array<Array<any>>, row : number, column :numb
   = (map, row, column) => row == map.length - 1;
 export const isInside : (map : Array<Array<any>>, row : number, column :number) => boolean 
   = (map, row, column) => !isRightSide(map, row, column) && !isLeftSide(map, row, column) && !isTopSide(map, row, column) && !isBottomSide(map, row, column);
+export const isCorner : (map : Array<Array<any>>, row : number, column :number) => boolean 
+  = (map, row, column) => (isRightSide(map, row, column) || isLeftSide(map, row, column)) && (isTopSide(map, row, column) || isBottomSide(map, row, column));
+
+export const isTopLeftCorner : (map : Array<Array<any>>, row : number, column :number) => boolean
+	= (map, row, column) => isTopSide(map, row, column) && isLeftSide(map, row, column);
+export const isTopRightCorner : (map : Array<Array<any>>, row : number, column :number) => boolean
+	= (map, row, column) => isTopSide(map, row, column) && isRightSide(map, row, column);
+export const isBottomLeftCorner : (map : Array<Array<any>>, row : number, column :number) => boolean
+	= (map, row, column) => isBottomSide(map, row, column) && isLeftSide(map, row, column);
+export const isBottomRightCorner : (map : Array<Array<any>>, row : number, column :number) => boolean
+	= (map, row, column) => isBottomSide(map, row, column) && isRightSide(map, row, column);
 
 export const getRandomInt : (from : number, lessThan :number) => number 
   = (from, to) => Math.floor(Math.random() * (to - from)) + from;
+
