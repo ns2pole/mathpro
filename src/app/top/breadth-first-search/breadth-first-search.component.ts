b90f8715b12a395135dde70a542813ea64a240ac
ns2pole <spaceofstar2@gmail.com>
2023-02-01 23:17:00 +0900

    動いた。

--- src/app/top/breadth-first-search/breadth-first-search.component.ts
+++ src/app/top/breadth-first-search/breadth-first-search.component.ts
@@ -13,10 +13,39 @@ import { getNumsIncludedIn, sleep } from './FunctionModule';
 })
 
 export class BreadthFirstSearchComponent {
+  lab : Labyrinth2D = Labyrinth2D.generateLabyrinth();
+  ad : AdjacentMatrix = AdjacentMatrix.getAdjacentMatrixFor(this.lab);
+  plane : Plane = new Plane(this.lab, this.ad);
+  sketch = (p: p5) => {
+    p.setup = () => {
+      let canvas = p.createCanvas(getCanvasWidth(p), getCanvasHeight(p));
+      let ele : any = document.getElementById('canvas');
+      canvas.parent(ele);
+      const startId : number = this.plane.lab.getStartId();
+      const goalId : number = this.plane.lab.getGoalId();
+      let sequence : Set<Array<number>> = new Set([[startId]]);
+      let searchedIds : Set<number> = getNumsIncludedIn(sequence);
+      // while(!searchedIds.has(goalId)) {
+      //   sequence = plane.evolute(sequence);
+      //   searchedIds = getNumsIncludedIn(sequence);
+      //   plane.lab.updateSearchedStatusBy(searchedIds);
+      //   p.draw = plane.draw(p);
+      // }
+      // const solutionPathId : Array<number> = plane.breadthFirstSearch();
+      // plane.lab.updateSearchedStatusBy(searchedIds);
+      // plane.lab.updateShortestPathBy(solutionPathId);
+      p.draw = this.plane.draw(p);
+      p.noLoop();
+    };
+
+    p.windowResized = () => {
+      p.resizeCanvas(getCanvasWidth(p), getCanvasHeight(p));
+    };
+  }
+  p : any = null;
   ngOnInit() {
-    const lab : Labyrinth2D = Labyrinth2D.generateLabyrinth();
-    const ad : AdjacentMatrix = AdjacentMatrix.getAdjacentMatrixFor(lab);
-    const plane : Plane = new Plane(lab, ad);
+    this.p = new p5(this.sketch);
+
     // const solutionPath : Array<number> = plane.breadthFirstSearch();
     //TODO:
     //ボタンを押すとスタートとゴールとラビリンスのみが描かれる(黄色, 緑, 黒)
@@ -33,32 +62,36 @@ export class BreadthFirstSearchComponent {
     //ボタンで段階的にアニメーション
     //解なし対応
     //mapサイズ変更しやすいよう対応
-    let sketch = (p: p5) => {
-      p.setup = () => {
-        let canvas = p.createCanvas(getCanvasWidth(p), getCanvasHeight(p));
-        let ele : any = document.getElementById('canvas');
-        canvas.parent(ele);
-        const startId : number = plane.lab.getStartId();
-        const goalId : number = plane.lab.getGoalId();
-        let sequence : Set<Array<number>> = new Set([[startId]]);
-        let searchedIds : Set<number> = getNumsIncludedIn(sequence);
-        while(!searchedIds.has(goalId)) {
-          sequence = plane.evolute(sequence);
-          searchedIds = getNumsIncludedIn(sequence);
-          plane.lab.updateSearchedStatusBy(searchedIds);
-          p.draw = plane.draw(p);
-        }
-        const solutionPathId : Array<number> = plane.breadthFirstSearch();
-        plane.lab.updateSearchedStatusBy(searchedIds);
-        plane.lab.updateShortestPathBy(solutionPathId);
-        // p.draw = plane.draw(p);
-        p.noLoop();
-      };
-
-      p.windowResized = () => {
-        p.resizeCanvas(getCanvasWidth(p), getCanvasHeight(p));
-      };
-    }
-    new p5(sketch);
   }
+
+  async draw() {
+    await this.evoluteAndDraw();
+    this.drawSolution();
+  }
+
+  async evoluteAndDraw() {
+    const startId : number = this.plane.lab.getStartId();
+    const goalId : number = this.plane.lab.getGoalId();
+    let sequence : Set<Array<number>> = new Set([[startId]]);
+    let searchedIds : Set<number> = getNumsIncludedIn(sequence);
+    while(!searchedIds.has(goalId)) {
+      sequence = this.plane.evolute(sequence);
+      searchedIds = getNumsIncludedIn(sequence);
+      this.plane.lab.updateSearchedStatusBy(searchedIds);
+      this.p.draw();
+      this.p.noLoop();
+      await sleep(100);
+    }
+  };
+
+  drawSolution() {
+    const solutionPathId : Array<number> = this.plane.breadthFirstSearch();
+    this.plane.lab.updateShortestPathBy(solutionPathId);
+    this.p.draw();
+    this.p.noLoop();
+  };
 }
+    // sequence = plane.evolute(sequence);
+    // let searchedIds : Set<number> = getNumsIncludedIn(sequence);
+    // plane.lab.updateSearchedStatusBy(searchedIds);
+    // p.draw = plane.draw(p);
