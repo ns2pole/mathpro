1c974d17c36b46049d8a195f947313f14862fe8e
ns2pole <spaceofstar2@gmail.com>
2023-02-03 18:11:24 +0900

    css修正

--- src/app/top/breadth-first-search/breadth-first-search.component.ts
+++ src/app/top/breadth-first-search/breadth-first-search.component.ts
@@ -16,55 +16,28 @@ export class BreadthFirstSearchComponent {
   lab : Labyrinth2D = Labyrinth2D.generateLabyrinth();
   ad : AdjacentMatrix = AdjacentMatrix.getAdjacentMatrixFor(this.lab);
   plane : Plane = new Plane(this.lab, this.ad);
+
   sketch = (p: p5) => {
     p.setup = () => {
       let canvas = p.createCanvas(getCanvasWidth(p), getCanvasHeight(p));
       let ele : any = document.getElementById('canvas');
       canvas.parent(ele);
-      const startId : number = this.plane.lab.getStartId();
-      const goalId : number = this.plane.lab.getGoalId();
-      let sequence : Set<Array<number>> = new Set([[startId]]);
-      let searchedIds : Set<number> = getNumsIncludedIn(sequence);
-      // while(!searchedIds.has(goalId)) {
-      //   sequence = plane.evolute(sequence);
-      //   searchedIds = getNumsIncludedIn(sequence);
-      //   plane.lab.updateSearchedStatusBy(searchedIds);
-      //   p.draw = plane.draw(p);
-      // }
-      // const solutionPathId : Array<number> = plane.breadthFirstSearch();
-      // plane.lab.updateSearchedStatusBy(searchedIds);
-      // plane.lab.updateShortestPathBy(solutionPathId);
-      p.draw = this.plane.draw(p);
-      p.noLoop();
-    };
 
+    };
+    p.draw = this.plane.draw(p);
+    p.noLoop();
     p.windowResized = () => {
       p.resizeCanvas(getCanvasWidth(p), getCanvasHeight(p));
     };
   }
   p : any = null;
+
   ngOnInit() {
     this.p = new p5(this.sketch);
-
-    // const solutionPath : Array<number> = plane.breadthFirstSearch();
-    //TODO:
-    //ボタンを押すとスタートとゴールとラビリンスのみが描かれる(黄色, 緑, 黒)
-    // plane.draw(p);
-    //drawSolutionをやめてSpaceに状態を持たせる。つまり、SpaceにisSearched, isOnSolutionを追加して描画はdraw(p)に統一。
-    //planeが持つ更新される。
-    //ボタンを押すと水が満たされていく様子が描かれる(水色)
-    //plane1 = getCopiedPlaneOverwrittenFor(searchedIds);
-    //plane1.draw(p);
-    //ボタンを押すと解が描かれる(赤)
-    //p2 = getCopiedPlaneOverwrittenFor(solutionPath);
-    //plane2.draw(p);
-    //TODO: 以下のようにする
-    //ボタンで段階的にアニメーション
-    //解なし対応
-    //mapサイズ変更しやすいよう対応
   }
 
   async draw() {
+    console.log('draw');
     await this.evoluteAndDraw();
     this.drawSolution();
   }
@@ -90,8 +63,13 @@ export class BreadthFirstSearchComponent {
     this.p.draw();
     this.p.noLoop();
   };
+
+
+  reset() {
+    this.lab = Labyrinth2D.generateLabyrinth();
+    this.ad = AdjacentMatrix.getAdjacentMatrixFor(this.lab);
+    this.plane = new Plane(this.lab, this.ad);
+    this.p = new p5(this.sketch);
+    document.getElementsByClassName('p5Canvas')[0].remove();
+  };
 }
-    // sequence = plane.evolute(sequence);
-    // let searchedIds : Set<number> = getNumsIncludedIn(sequence);
-    // plane.lab.updateSearchedStatusBy(searchedIds);
-    // p.draw = plane.draw(p);
