ac6da8b96556e487e6b85ab6bc7663ea3d7f9869
ns2pole <nakamura@feliseed.co.jp>
2023-02-18 23:25:09 +0900

    phpを試す

--- src/app/contact/test.php
+++ src/app/contact/test.php
@@ -1,13 +1,7 @@
 <?php
 // クエリ情報nameを取得
 $r = $_GET['name'];
-// クエリ情報nameが空の場合はエラー
-if (empty($r)) {
-  header('HTTP/1.1 500 Internal Server Error');
-} else {
   // 空でなければ、メッセージを出力
-  print('Hello,'.$r.'！');
+  header('Hello,'.$r.'！');
   $cmd = 'echo "これはテストです" | mail -s "テスト" spaceofstar2@gmail.com';
   exec($cmd);
-
-}
