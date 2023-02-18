2c6942e05d0033a8bb4375026f8c5b60e51a631a
ns2pole <nakamura@feliseed.co.jp>
2023-02-18 23:34:25 +0900

    引き続き試している

--- src/app/contact/test.php
+++ src/app/contact/test.php
@@ -5,3 +5,10 @@ $r = $_GET['name'];
   header('Hello,'.$r.'！');
   $cmd = 'echo "これはテストです" | mail -s "テスト" spaceofstar2@gmail.com';
   exec($cmd);
+  $email = "spaceofstar2@gmail.com";
+  $subject = "テスト"; // 題名
+  $body = "これはテストです。\n"; // 本文
+  $to = 'yyyyy@example.com';
+  $header = "From: $email\nReply-To: $email\n";
+  mb_send_mail($to, $subject, $body, $header);
+?>
